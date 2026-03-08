f94101d56c8cad5cf4406827b7e1e19b322b2c37
Flavioio <canovi.2067464@studneit.uniroma1.it>
2026-03-08 17:04:08 +0100

    cambio solo se serve

--- source/backend/automation/automation_engine.py
+++ source/backend/automation/automation_engine.py
@@ -1,6 +1,6 @@
 import crud
 from models import StandardFormat
-from cache import latest_sensor_state
+from cache import latest_sensor_state, latest_actuator_state
 from broker_client import BrokerClient
 
 command_broker = BrokerClient(host='activemq') # 'activemq' su Docker
@@ -17,11 +17,43 @@ def receive_event(event: StandardFormat):
     rules = crud.get_rules()
 
 
-    #print(event.value, event.metric, event.id)
+    print(event.value, event.metric, event.id)
 
     for r in rules:
 
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+        print("sto cercando le regole")
+
+        print(r.sensor_name, event.id, r.metric, event.metric)
+        print(r.sensor_name, event.id, r.metric, event.metric)
+        print(r.sensor_name, event.id, r.metric, event.metric)
+        print(r.sensor_name, event.id, r.metric, event.metric)
+        print(r.sensor_name, event.id, r.metric, event.metric)
+        print(r.sensor_name, event.id, r.metric, event.metric)
+        print(r.sensor_name, event.id, r.metric, event.metric)
+        print(r.sensor_name, event.id, r.metric, event.metric)
+
         if (r.sensor_name == event.id and r.metric == event.metric):
+
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
+            print("sono dentro all'if!!!")
 
             condition = False
 
@@ -38,19 +70,58 @@ def receive_event(event: StandardFormat):
             
             if condition:
                 print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
+                print(f"rule {r.id_rule} is now active!!")
 
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
+                print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
                 print(f"Motivo: {event.id} è {event.value} (Limite: {r.value})")
 
                 trigger_actuator(r.actuator_name, r.state)
     
-    print("fine elaborazione")
 
 def trigger_actuator(name: str, state: str):
     print(f"sending to actuator {name} command {state}")
 
+    if latest_actuator_state[name] == state:
+        return
+
     message = f'{{"actuator": "{name}", "state": "{state}"}}'
+
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
+    print(f"mandato messaggio: {name}, {state}")
 
     if command_broker.connected:
         command_broker.send_message("actuator_command", message)
+
+        latest_actuator_state[name] = state
+
+        print(f"stato di {name} cambiato a {state}")
     else:
         print("broker non connesso")
