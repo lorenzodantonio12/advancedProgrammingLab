f46d09eed74caf1f9a1202e9cafa5beeb176c74e
Flavioio <canovi.2067464@studneit.uniroma1.it>
2026-03-10 21:01:07 +0100

    try/catch aggiunti

--- source/backend/automation/database.py
+++ source/backend/automation/database.py
@@ -10,17 +10,21 @@ DATA_DIR.mkdir(parents = True, exist_ok = True)
 db = str(DATA_DIR / "mars_rules.db")
 
 def create_database():
-    conn = sqlite3.connect(db)
+    try:
 
-    cursor = conn.cursor()
+        with sqlite3.connect(db) as conn:
 
-    cursor.execute("""CREATE TABLE IF NOT EXISTS rules (
-                id_rule INTEGER PRIMARY KEY AUTOINCREMENT,
-                sensor_name TEXT NOT NULL,
-                operator TEXT NOT NULL,
-                value REAL NOT NULL,
-                metric TEXT NOT NULL,
-                actuator_name TEXT NOT NULL,
-                state TEXT)""")
+            cursor = conn.cursor()
 
-    conn.commit()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS rules (
+                        id_rule INTEGER PRIMARY KEY AUTOINCREMENT,
+                        sensor_name TEXT NOT NULL,
+                        operator TEXT NOT NULL,
+                        value REAL NOT NULL,
+                        metric TEXT NOT NULL,
+                        actuator_name TEXT NOT NULL,
+                        state TEXT)""")
+
+            conn.commit()
+    except sqlite3.Error as e:
+        print(f"❌ Errore critico durante la creazione del database: {e}")
